72d06251255d0226f93bf394d63eb685646c328d
coderaiser <coderaiser@cloudcmd.io>
2018-03-05 14:22:04 +0200

    feature(package) webpack v4.1.0

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@
 const path = require('path');
 const webpack = require('webpack');
 const {optimize} = webpack;
-const {UglifyJsPlugin} = optimize;
 
 const dir = './lib';
 
@@ -16,14 +15,7 @@ const devtool = 'source-map';
 const notEmpty = (a) => a;
 const clean = (array) => array.filter(notEmpty);
 
-const plugins = clean([
-    !isDev && new UglifyJsPlugin({
-        sourceMap: true,
-        comments: false,
-    }),
-]);
-
-const loaders = [{
+const rules = [{
     test: /\.js$/,
     exclude: /node_modules/,
     loader: 'babel-loader',
@@ -44,9 +36,8 @@ module.exports = {
         libraryTarget: 'var',
         devtoolModuleFilenameTemplate,
     },
-    plugins,
     module: {
-        loaders,
+        rules,
     },
 };
 
